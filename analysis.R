cb331bb2e76c8010c61e8de4b4383cccf99d2dbd
Jennifer Qin <jenniferqin@Jennifers-MacBook-Air.local>
2020-02-29 15:30:54 -0800

    Minor Changes to Plotting

--- analysis.R
+++ analysis.R
@@ -73,13 +73,11 @@ expectancy_spend <- arrange(expectancy, iso3c) %>%
   head(30)
   
 
-
 Country_spend <-ggplot(data = expectancy_spend, mapping = aes(x = reorder(iso3c, spending), y = spending)) + 
   geom_col()+
   labs(title= "Health Care Expenditures per Country", x = "Country", y = "Spending(% of GDP)")+
   theme(axis.text.x = element_text(size= 5,angle= 90))
 
-  
   
 #Compares Health Care Spending(% of GDP) to Healthy Life Expectancy for High income countries
 Health_plot <- ggplot(data = expectancy, mapping = aes(x = spending, y = Avg_life_expectancy))+
@@ -87,7 +85,6 @@ Health_plot <- ggplot(data = expectancy, mapping = aes(x = spending, y = Avg_lif
   geom_smooth(method = "lm" , formula = y~x)+
   labs(title= "Health Care Expenditures Compared to Healthy Life Expectancy", x = "Average Spending(% of GDP)", y = "Healthy Life Expectancy")+
   xlim(1,17)
-
 
 
 # Tony Section
