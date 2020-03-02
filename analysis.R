9971a635430714d79b283ea4bad747ac375eed0b
Tony Choi <tony@Tonys-MacBook-Pro.local>
2020-03-01 16:20:04 -0800

    Tony

--- analysis.R
+++ analysis.R
@@ -19,3 +19,29 @@ library(wbstats)
 
 
 # Tony Section
+
+us_debt_df <- wb(country = "USA",
+                 indicator = c("GC.DOD.TOTL.GD.ZS"),
+                 mrv = 20,
+                 return_wide = TRUE)
+
+us_debt_preanalysis <- us_debt_df %>%
+  filter(date >= 2006) %>%
+  mutate(debt = GC.DOD.TOTL.GD.ZS, date = as.numeric(date)) %>%
+  select(date, debt)
+
+us_debt_summary <- us_debt_preanalysis %>%
+  summarize(mean = mean(debt),
+            range = us_debt_preanalysis[11, "debt"] - us_debt_preanalysis[1, "debt"])
+
+us_debt_correlation <- cor(us_debt_preanalysis)
+
+us_debt_over_time_plot <- ggplot(data =  us_debt_preanalysis) +
+  geom_line(mapping = aes(x = date, y = debt)) +
+  geom_point(mapping = aes(x = date, y = debt)) +
+  labs(title = "US Debt Over Time",
+       x = "Year",
+       y = "Debt Percentage of GDP")
+
+us_debt_preanalysis[1, "debt"]
+us_debt_preanalysis[11, "debt"]
